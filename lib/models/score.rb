87644ae568c6fb9e781ab41f17e4c577301ceb7d
b <a@b.com>
2015-04-17 11:55:26 +0200

    added updating of leaderboards in Redis when Score model is saved

--- lib/models/score.rb
+++ lib/models/score.rb
@@ -18,42 +18,86 @@ module HighScore
       validates_numericality_of :score, greater_than_or_equal_to: 0
 
       after_save do |document|
-        # update personal leaderboards
-        daily_key = Score.personal_board_key(document.player_id, document.game_id, document.created_at, 'daily')
-        weekly_key = Score.personal_board_key(document.player_id, document.game_id, document.created_at, 'weekly')
-        monthly_key = Score.personal_board_key(document.player_id, document.game_id, document.created_at, 'monthly')
+        self.update_leaderboards('personal')
+        self.update_leaderboards('game')
+      end
 
+      def update_leaderboards(type = 'personal')
+        keys = self.leaderboard_keys(type)
         redis = HighScore::Wrapper.redis
-        results = redis.multi do
-          redis.zadd(daily_key, document.score, document.player_id)
-          redis.zremrangbyrank(daily_key, 0, -Global.leaderboard.personal_limit)
-          redis.zadd(weekly_key, document.score, document.player_id)
-          redis.zremrangbyrank(weekly_key, 0, -Global.leaderboard.personal_limit)
-          redis.zadd(monthly_key, document.score, document.player_id)
-          redis.zremrangbyrank(monthly_key, 0, -Global.leaderboard.personal_limit)
-        end
 
-        # update game leaderboards
-      end
-
-      def self.game_board_key(game_id, created_at, type = 'daily')
-        type = 'daily' unless ['daily', 'weekly', 'monthly'].include?(type)
-
-        if type == 'monthly'
-          "scoreboard:#{type}:#{created_at.year}-#{created_at.month}:#{game_id}"
-        elsif type == 'weekly'
-          week_in_year = created_at.strftime('%W')
-          "scoreboard:#{type}:#{created_at.year}-#{week_in_year}:#{game_id}"
-
+        if type == 'personal'
+          # cap the personal leaderboards according to the
+          # limit from the config to avoid them growing
+          # huge for prolific players
+          size_limit = Global.leaderboard.personal_limit
+          results = redis.multi do
+            redis.zadd(keys[:daily], self.score, self.created_at)
+            redis.zremrangbyrank(keys[:daily], 0, -size_limit)
+            redis.zadd(keys[:weekly], self.score, self.created_at)
+            redis.zremrangbyrank(keys[:weekly], 0, -size_limit)
+            redis.zadd(keys[:monthly], self.score, self.created_at)
+            redis.zremrangbyrank(keys[:monthly], 0, -size_limit)
+          end
         else
-          "scoreboard:#{type}:#{created_at.year}-#{created_at.month}-#{created_at.day}:#{game_id}"
-        end
-      end 
+          # sorted sets still track unique values, so combine
+          # player_id and created_at to ensure one player can
+          # hold multiple slots on the leaderboard
+          add_value = "#{self.player_id}-#{self.created_at}"
 
-      def self.personal_board_key(player_id, game_id, created_at, type = 'daily')
-        "#{self.game_board_key(game_id, created_at, type)}:#{player_id}"
+          results = redis.multi do
+            redis.zadd(keys[:daily], self.score, add_value)
+            redis.zadd(keys[:weekly], self.score, add_value)
+            redis.zadd(keys[:monthly], self.score, add_value)
+          end
+        end
       end
 
+      def leaderboard_keys(type = 'personal')
+        ['player_id', 'game_id', 'created_at'].each do |key|
+          raise KeyError.new("#{key} is required to generate leaderboard keys") unless self.send(key)
+        end
+        week_in_year = self.created_at.strftime('%W')
+
+        if type == "game"
+          {
+            :daily => [
+              "scoreboard:daily",
+              [
+                created_at.year,
+                created_at.month,
+                created_at.day,
+              ].join('-'),
+              self.game_id,
+            ].join(':'),
+
+            :weekly => [
+              "scoreboard:weekly",
+              [
+                created_at.year,
+                week_in_year,
+              ].join('-'),
+              self.game_id,
+            ].join(':'),
+
+            :monthly => [
+              "scoreboard:monthly",
+              [
+                created_at.year,
+                created_at.month,
+              ].join('-'),
+              self.game_id,
+            ].join(':'),
+          }
+        else
+          game_keys = self.leaderboard_keys('game')
+          {
+            :daily => "#{game_keys[:daily]}:#{self.player_id}",
+            :weekly => "#{game_keys[:weekly]}:#{self.player_id}",
+            :monthly => "#{game_keys[:monthly]}:#{self.player_id}",
+          }
+        end
+      end
     end
   end
 end
--- spec/highscore_model_spec.rb
+++ spec/highscore_model_spec.rb
@@ -8,76 +8,124 @@ describe HighScore::Models::Score do
     is_expected.to validate_numericality_of(:score)
   end
 
-  describe "class methods" do
-    describe "personal board key" do
-      it "creates a daily key" do
-        created_at = DateTime.new(2001,2,3,4,5,6)
-        score = HighScore::Models::Score.personal_board_key('a_player', 'a_game', created_at, 'daily')
-        expect( score ).to eq("scoreboard:daily:2001-2-3:a_game:a_player")
-      end
+  describe "after_create hook" do
+    it "inserts the score in the personal and game leaderboards" do
+      score = make_score
+      mock(score).update_leaderboards('personal')
+      mock(score).update_leaderboards('game')
+      score.save
+    end
+  end
 
-      it "creates a weekly key" do
-        created_at = DateTime.new(2001,2,3,4,5,6)
-        score = HighScore::Models::Score.personal_board_key('a_player', 'a_game', created_at, 'weekly')
-        expect( score ).to eq("scoreboard:weekly:2001-05:a_game:a_player")
-      end
-      
-      it "creates a monthly key" do
-        created_at = DateTime.new(2001,2,3,4,5,6)
-        score = HighScore::Models::Score.personal_board_key('a_player', 'a_game', created_at, 'monthly')
-        expect( score ).to eq("scoreboard:monthly:2001-2:a_game:a_player")
+  describe "#update_leaderboards" do
+    describe "personal leaderboards" do
+      it "inserts the score in redis using the time as a value and caps the number of recorded scores" do
+        created_at = Time.now
+        score = make_score(:created_at => created_at)
+
+        keys = score.leaderboard_keys('personal')
+
+        redis = HighScore::Wrapper.redis
+        stub( HighScore::Wrapper ).redis.returns(redis)
+
+        size_limit = Global.leaderboard.personal_limit
+        mock(redis).zadd(keys[:daily], score.score, score.created_at)
+        mock(redis).zremrangbyrank(keys[:daily], 0, -size_limit)
+        mock(redis).zadd(keys[:weekly], score.score, score.created_at)
+        mock(redis).zremrangbyrank(keys[:weekly], 0, -size_limit)
+        mock(redis).zadd(keys[:monthly], score.score, score.created_at)
+        mock(redis).zremrangbyrank(keys[:monthly], 0, -size_limit)
+
+        score.update_leaderboards('personal')
       end
     end
 
-    describe "game board key" do
-      it "creates a daily key" do
-        created_at = DateTime.new(2001,2,3,4,5,6)
-        score = HighScore::Models::Score.game_board_key('a_game', created_at, 'daily')
-        expect( score ).to eq("scoreboard:daily:2001-2-3:a_game")
-      end
+    describe "game leaderboards" do
+      it "inserts the score in redis using the combined player/time as a value so one player can hold multiple ranks" do
+        created_at = Time.now
+        score = make_score(:created_at => created_at)
 
-      it "creates a weekly key" do
-        created_at = DateTime.new(2001,2,3,4,5,6)
-        score = HighScore::Models::Score.game_board_key('a_game', created_at, 'weekly')
-        expect( score ).to eq("scoreboard:weekly:2001-05:a_game")
-      end
-      
-      it "creates a monthly key" do
-        created_at = DateTime.new(2001,2,3,4,5,6)
-        score = HighScore::Models::Score.game_board_key('a_game', created_at, 'monthly')
-        expect( score ).to eq("scoreboard:monthly:2001-2:a_game")
+        keys = score.leaderboard_keys('game')
+
+        redis = HighScore::Wrapper.redis
+        stub( HighScore::Wrapper ).redis.returns(redis)
+
+        add_value = "#{score.player_id}-#{score.created_at}"
+
+        mock(redis).zadd(keys[:daily], score.score, add_value)
+        mock(redis).zadd(keys[:weekly], score.score, add_value)
+        mock(redis).zadd(keys[:monthly], score.score, add_value)
+
+        score.update_leaderboards('game')
       end
     end
   end
 
-  describe "after save" do
-    describe "personal scoreboard" do
-      it "inserts the score in redis and caps the number of recorded scores" do
+  describe "#leaderboard_keys" do
+    describe "generates leaderboard keys for the current player" do
+      it "generates daily, weekly and monthly keys" do
         score = make_score
+        score.created_at = DateTime.new(2001,2,3,4,5,6)
+        keys = score.leaderboard_keys('personal')
 
-        created_at = Time.now
-        RR.stub(score).created_at { created_at }
+        expect( keys[:daily] ).to eq("scoreboard:daily:2001-2-3:some_game:some_player")
+        expect( keys[:weekly] ).to eq("scoreboard:weekly:2001-05:some_game:some_player")
+        expect( keys[:monthly] ).to eq("scoreboard:monthly:2001-2:some_game:some_player")
+      end
 
-        daily_key = HighScore::Models::Score.personal_board_key(score.player_id, score.game_id, score.created_at, 'daily')
-        weekly_key = HighScore::Models::Score.personal_board_key(score.player_id, score.game_id, score.created_at, 'weekly')
-        monthly_key = HighScore::Models::Score.personal_board_key(score.player_id, score.game_id, score.created_at, 'monthly')
+      it "fails if the game_id is not present" do
+        score = make_score(:game_id => nil)
+        expect do
+          score.leaderboard_keys('personal')
+        end.to raise_error(KeyError, "game_id is required to generate leaderboard keys")
+      end
 
-        redis = HighScore::Wrapper.redis
+      it "fails if the player_id is not present" do
+        score = make_score(:player_id => nil)
+        expect do
+          score.leaderboard_keys('personal')
+        end.to raise_error(KeyError, "player_id is required to generate leaderboard keys")
+      end
 
-        RR.mock(redis).zadd(daily_key, score.score, score.player_id)
-        RR.mock(redis).zremrangbyrank(daily_key, 0, -Global.leaderboard.personal_limit)
-        RR.mock(redis).zadd(weekly_key, score.score, score.player_id)
-        RR.mock(redis).zremrangbyrank(weekly_key, 0, -Global.leaderboard.personal_limit)
-        RR.mock(redis).zadd(monthly_key, score.score, score.player_id)
-        RR.mock(redis).zremrangbyrank(monthly_key, 0, -Global.leaderboard.personal_limit)
-        
-        RR.stub( HighScore::Wrapper ).redis.returns(redis)
-
-        make_score.save
+      it "fails if there is no created_at time recorded" do
+        score = make_score
+        expect do
+          score.leaderboard_keys('personal')
+        end.to raise_error(KeyError, "created_at is required to generate leaderboard keys")
       end
     end
 
-    it "updates the game's scoreboards" do
+    describe "generates leaderboard keys for the current game" do
+      it "generates daily, weekly and monthly keys" do
+        score = make_score
+        score.created_at = DateTime.new(2001,2,3,4,5,6)
+        keys = score.leaderboard_keys('game')
+
+        expect( keys[:daily] ).to eq("scoreboard:daily:2001-2-3:some_game")
+        expect( keys[:weekly] ).to eq("scoreboard:weekly:2001-05:some_game")
+        expect( keys[:monthly] ).to eq("scoreboard:monthly:2001-2:some_game")
+      end
+
+      it "fails if the game_id is not present" do
+        score = make_score(:game_id => nil)
+        expect do
+          score.leaderboard_keys('game')
+        end.to raise_error(KeyError, "game_id is required to generate leaderboard keys")
+      end
+
+      it "fails if the player_id is not present" do
+        score = make_score(:player_id => nil)
+        expect do
+          score.leaderboard_keys('game')
+        end.to raise_error(KeyError, "player_id is required to generate leaderboard keys")
+      end
+
+      it "fails if there is no created_at time recorded" do
+        score = make_score
+        expect do
+          score.leaderboard_keys('game')
+        end.to raise_error(KeyError, "created_at is required to generate leaderboard keys")
+      end
     end
   end
 
